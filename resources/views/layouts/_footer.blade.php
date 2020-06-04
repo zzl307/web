13ebbe5da431540e41f36f541aff4364277707bb
vagrant <vagrant@homestead>
2020-06-04 06:30:21 +0000

    web

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,21 +3,21 @@
         <div class='qualContent absolute'>
             <div class='qualfl fl'>
                 <img style="width: 277px; height: 40px;" src="{{ asset('image/logo.png') }}" alt="">
-                <div class='qualityTxt1'><em class='iBlock fourIcon'></em> 0551-66666608</div>
-                <div class='qualityTxt2'><em class='iBlock fourIcon'></em> 门诊时间: 9:00-20:00</div>
-                <p>帮助每一位爱美的朋友,安全放心的</p>
+                <div class='qualityTxt1'><em class='iBlock fourIcon'></em> 025-8420-9537</div>
+                <div class='qualityTxt2'><em class='iBlock fourIcon'></em> 门诊时间: 9:00-19:00</div>
+                <p>为万千求美者提供安全、安心的美丽体验</p>
             </div>
             <div class='qualCen fl'>
-                <img src='{{ asset('image/65a9dd16ee22ed2e74322f6949e3dcc.jpg') }}' />
+                <img style="width: 162px;height: 162px;" src='{{ asset('image/65a9dd16ee22ed2e74322f6949e3dcc.jpg') }}' />
             </div>
             <div class='qualLinks fl clearfix'>
                 <a href='#' class='iBlock fl boxIndex'>联系我们</a>
-            </div>	
+            </div>
         </div>
     </div>
 </section>
 <section class='footerIndex' id='footerBz'>
     <div class='footerCon wPub'>
-        <p>网站备案号 <a href="http://beian.miit.gov.cn/state/outPortal/loginPortal.action">苏ICP备09087349号-3</a> <br/><a href="{{ route('sitemap.index') }}">网站地图</a><br/>详情请到医院资讯</p><br/>
+        <p>网站备案号 <a href="http://beian.miit.gov.cn/state/outPortal/loginPortal.action">苏ICP备09087349号-3</a> <br/><a href="{{ route('sitemap.index') }}">网站地图</a><br/>地址：江苏省南京市秦淮区中山南路321号现代大厦2楼<br/>详情请到医院资讯</p><br/>
     </div>
 </section>
